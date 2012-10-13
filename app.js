ae5895ec1d8354e913212e2f8a99db7910208389
pringshia <pratik@ringshia.com>
2012-10-13 19:14:15 -0400

    Format results

--- app.js
+++ app.js
@@ -3,17 +3,42 @@ var app = express();
 
 
 app.get('/', function(req, res) {
-    var request = require('request');
-    var query = req.query['search'] || 'comedy';
-    var duration = req.query['duration'] || 'long';
-    request('https://gdata.youtube.com/feeds/api/videos?alt=json&q='+query+'&duration='+duration+'&v=2', function (error, response, body) {
-	if (!error && response.statusCode == 200) {
-	    var json = JSON.parse(body);
-	    var page = json.feed.entry;
-	    res.send(page);
-	}
-    })
+	var request = require('request');
+	var query = req.query['search'] || 'comedy';
+	var time_remaining = req.query['duration'] || '300';
+	
+	if (time_remaining < 240)
+		duration = 'short';
+	else if (time_remaining <= 1200)
+		duration = 'medium';
+	else
+		duration = 'long';
+	
+	request('https://gdata.youtube.com/feeds/api/videos?alt=json&q='+query+'&duration='+duration+'&v=2', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			//get json data
+			var json = JSON.parse(body);
+
+			//determine what to output
+			var videos = json.feed.entry;
+			var page = "<ul>";
+			for (var i = 0; i < videos.length; i++)
+			{
+				var video = videos[i];
+				var duration = video['media$group']['yt$duration']['seconds'];
+				var title = video['title']['$t'];
+				var link = video['link'][0]['href'];
+				
+				console.log(duration, title, link);
+				
+				page += "<li><a href='"+link+"'>"+title+"</a> - "+duration+" seconds</li>";
+			}
+			page += "</ul>";
+			res.send(page);
+		}
+	});
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var PORTNUMBER = 3000;
+app.listen(PORTNUMBER);
+console.log('Listening on port '+PORTNUMBER);
