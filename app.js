db8cf3042d49172321c97ce9341e844c875c6038
pringshia <pratik@ringshia.com>
2012-10-13 18:09:34 -0400

    Add main app.js file that queries Youtube

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res) {
+    var request = require('request');
+    var query = req.query['search'] || 'comedy';
+    var duration = req.query['duration'];
+    request('https://gdata.youtube.com/feeds/api/videos?alt=json&q='+query+'&duration='+duration+'&v=2', function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+	    var json = JSON.parse(body);
+	    var page = json.feed.entry;
+	    res.send(page);
+	}
+    })
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
