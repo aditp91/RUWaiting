6b1badea05a43678e8ba4fb81e1120d109ce0aae
pringshia <pratik@ringshia.com>
2012-10-13 18:33:58 -0400

    Add request and default video length

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.get('/', function(req, res) {
     var request = require('request');
     var query = req.query['search'] || 'comedy';
-    var duration = req.query['duration'];
+    var duration = req.query['duration'] || 'long';
     request('https://gdata.youtube.com/feeds/api/videos?alt=json&q='+query+'&duration='+duration+'&v=2', function (error, response, body) {
 	if (!error && response.statusCode == 200) {
 	    var json = JSON.parse(body);
