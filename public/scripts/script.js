ab9457951e152b0b36c53b312c9a523e7a1832f8
pringshia <pratik@ringshia.com>
2012-10-13 23:08:21 -0400

    Make ajax calls normal

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -12,27 +12,25 @@ function begin()
 			
 			$.ajax('nearby/?lat='+lat+'&lon='+lng, {
 				success: function(data, status, xhr) {
-					console.log(data);					
+					console.log(data);
+					//data = ['College Hall', 'Scott Hall', 'Trayes Hall'];
+					for (i=0;i<data.length;i++){
+					   $('<option/>').val(data[i]).html(data[i]).appendTo('#stop');
+					}				
 				}
 			});
-			
-			data = ['College Hall', 'Scott Hall', 'Trayes Hall'];
-			for (i=0;i<data.length;i++){
-			   $('<option/>').val(data[i]).html(data[i]).appendTo('#stop');
-			}
 			
 			$('#stop').change(function(e) {
 				$.ajax('buses/?stop='+$('#stop').val(), {
 					success: function(data, status, xhr) {
-						console.log(data);					
+						console.log(data);		
+						//data = [{title: 'EE', seconds: '100'}, {title: 'A', seconds: '340'}];
+						for (i=0;i<data.length;i++){
+						   $('<option/>').val(data[i].seconds).html(data[i].title).appendTo('#bus');
+						}
 					}
 				});
 			});
-			
-			data = [{title: 'EE', seconds: '100'}, {title: 'A', seconds: '340'}];
-			for (i=0;i<data.length;i++){
-			   $('<option/>').val(data[i].seconds).html(data[i].title).appendTo('#bus');
-			}
 			
 		}, function(err) {
 			if (err.code == 1)
